028c3b5a591aef6d475afcbd659e0be04aa1125c
Donavon Buchanan <5569820+dbuchanandev@users.noreply.github.com>
2023-05-08 02:20:42 -0500

    Add default localization

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "CLImageEditor",
+    defaultLocalization: "en",
     platforms: [.iOS(.v15), .macOS(.v12), .macCatalyst(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
