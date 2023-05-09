665a121de141aada3dc3b47e285963a3b6ef6114
Donavon Buchanan <5569820+dbuchanandev@users.noreply.github.com>
2023-05-09 01:09:22 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,23 +4,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "CLImageEditor",
-    defaultLocalization: "en",
-    platforms: [.iOS(.v15), .macOS(.v12), .macCatalyst(.v15)],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "CLImageEditor",
-            targets: ["CLImageEditor"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "CLImageEditor"),
-        .testTarget(
-            name: "CLImageEditorTests",
-            dependencies: ["CLImageEditor"]),
-    ],
-    swiftLanguageVersions: [.v5]
+  name: "CLImageEditor",
+  defaultLocalization: "en",
+  platforms: [.iOS(.v15)],
+  products: [
+    .library(
+      name: "CLImageEditor",
+      targets: ["CLImageEditor"]),
+  ],
+  targets: [
+    .target(
+      name: "CLImageEditor",
+      path: "Sources/CLImageEditor",
+      publicHeadersPath: ".",
+      cSettings: [
+        .headerSearchPath("."),
+      ]
+    )
+  ],
+  swiftLanguageVersions: [.v5]
 )
